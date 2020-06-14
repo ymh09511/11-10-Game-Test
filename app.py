d2740b6d48e4d40daa70c2506d16c6d2c223dbe4
Yoo Minho <ymh0951@naver.com>
2020-06-14 20:12:59 +0900

    sqlite db 소스 적용

--- app.py
+++ app.py
@@ -73,16 +73,15 @@ def method():
         num = request.form['num']
         name = request.form['name']
         print(num, name)
-        with open("static/save.txt", "w", encoding='utf-8') as f:
-            f.write("%s,%s" % (num, name))
+        dbdb.insert_data(num, name)
         return 'POST 이다. 학번은: {} 이름은: {}'.format(num, name)
 
 @app.route('/getinfo')
 def getinfo():
-    # 파일 입력
-    with open("static/save.txt", "r", encoding='utf-8') as file:
-        student = file.read().split(',') # 쉼표로 잘려서 student 에 배열로 저장
-    return '번호 : {}, 이름 : {}'.format(student[0], student[1])
+    ret = dbdb.select_all()
+    print(ret[4])
+    return render_template('getinfo.html', data=ret)
+    # return '번호 : {}, 이름 : {}'.format(ret[0], ret[1])
 
 @app.route('/naver')
 def naver():
